cb6e036e8f386bcd4f5b757797a86008734f967a
TatianeMarinho <devtatianemarinho@gmail.com>
2024-06-19 16:49:04 -0400

    navbar e seu css

--- src/components/NavBar/NavbarMenu.tsx
+++ src/components/NavBar/NavbarMenu.tsx
@@ -1,7 +1,16 @@
+import './navbarMenu.css';
+
 function NavbarMenu() {
     return (
         <nav>
-            <p>links</p>
+            <ul>
+                <li><a href="secao">Refeições</a></li>
+                <li><a href="secao">Comerciais</a></li>
+                <li><a href="secao">Porções</a></li>
+                <li><a href="secao">Massas</a></li>
+                <li><a href="secao">Bebidas</a></li>
+                <li><a href="secao">Sobremesas</a></li>
+            </ul>
         </nav>
     )
 }
